25bd97ab13c123307790e30040a8417f46bae730
theuitown <nitingoswami473@gmail.com>
2020-03-13 19:07:32 +0530

    Initial buid v1

--- script.py
+++ script.py
@@ -7,4 +7,10 @@ with open('walldrops-d3df3-export.json', 'r') as f:
 for item in data["Backgrounds"]:
     img_url = data["Backgrounds"][item]['imageLink']
     img_name = os.path.basename(img_url)
-    urllib.request.urlretrieve(img_url,'/home/iamharsh/Projects/Downloads_walls/wallpapers/'+img_name)
+    urllib.request.urlretrieve(img_url,'/home/iamharsh/Walldropsdatabase/wallpapers/'+img_name)
+    # newImageLink=
+    # tempfile = os.path.join(os.path.dirname('walldrops-d3df3-export.json'), str(uuid.uuid4()))
+    # with open(tempfile, 'w') as f:
+    #     json.dump(data, f, indent=4)
+    #               # rename temporary file replacing old file
+    # os.rename(tempfile, 'hotels.json')
