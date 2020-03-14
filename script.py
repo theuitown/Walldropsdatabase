d70758518c37a6f6054e4bef0769149a91857399
theuitown <nitingoswami473@gmail.com>
2020-03-14 14:54:57 +0530

    added all the walls

--- script.py
+++ script.py
@@ -1,16 +1,17 @@
 import urllib.request
 import os, json, uuid,sys
 
-with open('walldrops-d3df3-export.json', 'r') as f:
+with open('done.json', 'r') as f:
     data = json.load(f)
 
-for item in data["Backgrounds"]:
-    img_url = data["Backgrounds"][item]['imageLink']
+for item in data["CategoryBackground"]:
+    img_url = data["CategoryBackground"][item]['imageUrl']
     img_name = os.path.basename(img_url)
-    urllib.request.urlretrieve(img_url,'/home/iamharsh/Walldropsdatabase/wallpapers/'+img_name)
-    # newImageLink=
-    # tempfile = os.path.join(os.path.dirname('walldrops-d3df3-export.json'), str(uuid.uuid4()))
-    # with open(tempfile, 'w') as f:
-    #     json.dump(data, f, indent=4)
-    #               # rename temporary file replacing old file
-    # os.rename(tempfile, 'hotels.json')
+    print(json.dumps(data["CategoryBackground"][item]['imageUrl']))
+    urllib.request.urlretrieve(img_url,'/home/iamharsh/Walldropsdatabase/wallpapers/categories/'+img_name)
+    newImageLink='https://raw.githubusercontent.com/theuitown/Walldropsdatabase/master/wallpapers/categories/'+img_name
+    data["CategoryBackground"][item]['imageUrl']=newImageLink
+    tempfile = os.path.join(os.path.dirname('done.json'), str(uuid.uuid4()))
+    with open(tempfile, 'w') as f:
+        json.dump(data, f, indent=4)
+    os.rename(tempfile, 'done.json')
