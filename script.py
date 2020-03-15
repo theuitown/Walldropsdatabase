3e3d8bc49785fcf72d98428163ee2e940e8b1b31
theuitown <nitingoswami473@gmail.com>
2020-03-15 10:08:37 +0530

    some walls aded v1

--- script.py
+++ script.py
@@ -1,17 +1,19 @@
 import urllib.request
 import os, json, uuid,sys
 
-with open('done.json', 'r') as f:
+with open('walldrops-bck.json', 'r') as f:
     data = json.load(f)
 
-for item in data["CategoryBackground"]:
-    img_url = data["CategoryBackground"][item]['imageUrl']
-    img_name = os.path.basename(img_url)
-    print(json.dumps(data["CategoryBackground"][item]['imageUrl']))
-    urllib.request.urlretrieve(img_url,'/home/iamharsh/Walldropsdatabase/wallpapers/categories/'+img_name)
-    newImageLink='https://raw.githubusercontent.com/theuitown/Walldropsdatabase/master/wallpapers/categories/'+img_name
-    data["CategoryBackground"][item]['imageUrl']=newImageLink
-    tempfile = os.path.join(os.path.dirname('done.json'), str(uuid.uuid4()))
+i=133
+for item in data["Backgrounds"]:
+    img_url = data["Backgrounds"][item]['imageLink']
+    img_name = os.path.basename("walldrops"+str(i)+".jpg")
+    print(json.dumps(data["Backgrounds"][item]['imageLink']))
+    urllib.request.urlretrieve(img_url,'/home/iamharsh/Walldropsdatabase/wallpapers/'+img_name)
+    newImageLink='https://raw.githubusercontent.com/theuitown/Walldropsdatabase/master/wallpapers/'+img_name
+    data["Backgrounds"][item]['imageLink']=newImageLink
+    tempfile = os.path.join(os.path.dirname('walldrops-bck.json'), str(uuid.uuid4()))
     with open(tempfile, 'w') as f:
         json.dump(data, f, indent=4)
-    os.rename(tempfile, 'done.json')
+    os.rename(tempfile, 'walldrops-bck.json')
+    i+=1
