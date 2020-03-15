105594b68370e7bff2d7dd6111bb61f747a20ff4
theuitown <nitingoswami473@gmail.com>
2020-03-15 14:07:13 +0530

    some walls aded v2

--- script.py
+++ script.py
@@ -4,13 +4,13 @@ import os, json, uuid,sys
 with open('walldrops-bck.json', 'r') as f:
     data = json.load(f)
 
-i=133
+i=1088
 for item in data["Backgrounds"]:
     img_url = data["Backgrounds"][item]['imageLink']
     img_name = os.path.basename("walldrops"+str(i)+".jpg")
     print(json.dumps(data["Backgrounds"][item]['imageLink']))
-    urllib.request.urlretrieve(img_url,'/home/iamharsh/Walldropsdatabase/wallpapers/'+img_name)
-    newImageLink='https://raw.githubusercontent.com/theuitown/Walldropsdatabase/master/wallpapers/'+img_name
+    urllib.request.urlretrieve(img_url,'/home/iamharsh/Walldropsdatabase/wallpaperass/'+img_name)
+    newImageLink='https://raw.githubusercontent.com/theuitown/Walldropsdatabase/master/wallpaperass/'+img_name
     data["Backgrounds"][item]['imageLink']=newImageLink
     tempfile = os.path.join(os.path.dirname('walldrops-bck.json'), str(uuid.uuid4()))
     with open(tempfile, 'w') as f:
