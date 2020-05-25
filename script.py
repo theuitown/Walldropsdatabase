69bea8d830954fae2f8fea1717ac91385422453d
theuitown <nitingoswami473@gmail.com>
2020-05-25 17:57:33 +0530

    lol

--- script.py
+++ script.py
@@ -4,11 +4,9 @@ import os, json, uuid,sys
 with open('walldropswalls.json', 'r') as f:
     data = json.load(f)
 
-i=-1
 for item in data["Backgrounds"]:
-    data["Backgrounds"][item].update({'timestamp':i})
+    data["Backgrounds"][item].pop('timestamp')
     tempfile = os.path.join(os.path.dirname('walldropswalls.json'), str(uuid.uuid4()))
     with open(tempfile, 'w') as f:
         json.dump(data, f, indent=4)
     os.rename(tempfile, 'walldropswalls.json')
-    i-=1
